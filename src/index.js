c325af1a3a5bab91c7e1ec3f6977aa05e5354b56
Andrey <santa-andrio@mail.ru>
2023-02-02 22:40:05 +0300

    feat: Add toReadable function functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function toReadable (number) {
-  
+    const digits1 = ['zero', 'one', 'two', 'three', 'four', 'five', 'six',
+        'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen',
+        'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const digits2 = ['', 'ten', 'twenty', 'thirty', 'forty', 'fifty', 'sixty',
+        'seventy', 'eighty', 'ninety'];
+    
+    let result = ''
+    
+    if ( number === 0 ) result = digits1[0]
+    if ( number > 99 ) {
+        result += (digits1[Math.trunc(number / 100)] + ' hundred ');
+        number %= 100;
+    }
+    if ( number < 100 && number > 19 ) {
+        result += digits2[Math.trunc(number / 10)].concat(' ');
+        number %= 10;
+    }
+    if ( number > 0 ) result += digits1[number];
+
+    return result.trim();
 }
